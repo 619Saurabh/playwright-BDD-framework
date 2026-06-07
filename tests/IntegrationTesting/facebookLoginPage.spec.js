d4f33b53c39c6ec36416fbd0f627aed39453d65e
Saurabh Singh <saurabh26sep2000@gmail.com>
2026-06-07 22:28:14 +0530

    SDET B updated facebookLogin file

--- tests/IntegrationTesting/facebookLoginPage.spec.js
+++ tests/IntegrationTesting/facebookLoginPage.spec.js
@@ -1,5 +1,5 @@
 test('verify facebook title', async() => {
     await page.goto("https://www.facebook.com/");
     console.log(await page.title());
-    await expect(page).toHaveTitle("Hi I am Automation Engineer B")
+    await expect(page).toHaveTitle("Hi I am SDET B")
 })
